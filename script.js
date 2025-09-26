21a2add2e921e3d28977849ddb3aab6b3b0e9ff5
Abu Hanjala <abuhanjala656@gmail.com>
2025-09-26 23:57:15 +0530

    Add pomodore Timer

--- script.js
+++ script.js
@@ -32,4 +32,75 @@ function rippleEffect() {
     taskBtn.style.setProperty("--posY", y + "px");
   });
 }
- rippleEffect();
+rippleEffect();
+
+function pomodoreTimer() {
+  let time = document.querySelector(".pomo-timer p");
+  let startBtn = document.querySelector("#start-timer");
+  let pauseBtn = document.querySelector("#pause-timer");
+  let resetBtn = document.querySelector("#reset-timer");
+  let session = document.querySelector(".pomo-timer .session");
+
+  let totalSeconds = 25 * 60;
+  let timerInterval;
+  let isWorkSession = true;
+
+  function updateTime() {
+    let minute = Math.floor(totalSeconds / 60);
+    let second = totalSeconds % 60;
+    time.innerHTML = `${String(minute).padStart("2", "0")}:${String(
+      second
+    ).padStart("2", "0")}`;
+  }
+  // srtarTimer
+  const startTimer = () => {
+    clearInterval(timerInterval);
+    if (isWorkSession) {
+      timerInterval = setInterval(() => {
+        if (totalSeconds > 0) {
+          totalSeconds--;
+          updateTime();
+        } else {
+          isWorkSession = false;
+          clearInterval(timerInterval);
+          time.innerHTML = "05:00";
+          session.innerHTML = "Take a Break";
+          session.style.backgroundColor = "var(--text-input2)";
+          session.style.color = "var( --pomo-card)";
+          totalSeconds = 5 * 60;
+        }
+      }, 10);
+    } else {
+      timerInterval = setInterval(() => {
+        if (totalSeconds > 0) {
+          totalSeconds--;
+          updateTime();
+        } else {
+          isWorkSession = true;
+          clearInterval(timerInterval);
+          time.innerHTML = "25:00";
+          session.innerHTML = "Work Session";
+          session.style.backgroundColor = "var(--toggle-btn--circle)";
+          session.style.color = "var( --text-input2)";
+          totalSeconds = 25 * 60;
+        }
+      }, 10);
+    }
+  };
+
+  // pauseTimer
+  const pauseTimer = () => {
+    clearInterval(timerInterval);
+  };
+  // resetTimer
+  const resetTimer = () => {
+    clearInterval(timerInterval);
+    totalSeconds = isWorkSession ? 25 * 60 : 5 * 60;
+    updateTime();
+  };
+
+  startBtn.addEventListener("click", startTimer);
+  pauseBtn.addEventListener("click", pauseTimer);
+  resetBtn.addEventListener("click", resetTimer);
+}
+pomodoreTimer();
